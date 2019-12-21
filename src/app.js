b314aed28fe35b05400b2407ac58a6ce3c9d1044
ShotaTogawa <s.togawa0112@gmail.com>
2019-12-20 23:37:26 -0800

    added / infront of client

--- src/app.js
+++ src/app.js
@@ -25,13 +25,13 @@ app.use("/api", uploadRouter);
 if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets
   // like our main.js file, or main.css file!
-  app.use(express.static("client/build"));
+  app.use(express.static("/client/build"));
 
   // Express will serve up the index.html file
   // if it doesn't recoginize the route
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "/client", "build", "index.html"));
   });
 }
 
