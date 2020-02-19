402b8469fb8c61c5d41a02f6fa9c1e554d22f49e
boompay <curryisdrink.sb@gmail.com>
2020-02-19 09:24:53 +0200

    reviews

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import Home from "./components/home/Home";
 
+// package.json にコメント出来なかったので代わりにここに書くのですが
+// lodash もいいのですが、似たようなライブラリの ramda が個人的におすすめです
+// https://www.codementor.io/@michelre/functional-javascript-why-i-prefer-using-ramda-over-lodash-or-underscore-dzovysq11
+
 class App extends Component {
   render() {
     return <Home />;
--- client/src/spinner/Spinner.js
+++ client/src/spinner/Spinner.js
@@ -1,6 +1,8 @@
 import React from "react";
 import classes from "./spinner.css";
 
+// これはコンポーネントを返しているので、/spinner 配下のファイルはまとめて /components に合ったほうが適切に思います
+
 const Spinner = () => {
   return (
     <div className="Loader" style={classes.Loader}>
